2896dfd8d37ca71847d55d2ff7ec9f230aca87ea
polinabobrova <polinabobrova@tjcollection.ru>
2024-06-03 19:50:09 +0200

    Update Readme

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require('ethereum-cryptography/secp256k1');
+const {keccak256} = require('ethereum-cryptography/keccak');
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0216dc441b9251c68b2ffba1ce2651879016897a4e62d9cbeab88145613dedaeda": 100,
+  "0213bddfc5e6de5fb44f98070d6c1c55801af1f755c002008cf5a02a933ba44cf0": 50,
+  "02b08dd3819ac61817adb96c9de00cba69c4866bbc56a27cc37d800131a5258879": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +21,21 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, sig:sigStringed, msg } = req.body;
+  const { recipient, amount } = msg;
+
+  // convert stringified bigints back to bigints
+  const sig = {
+    ...sigStringed,
+    r: BigInt(sigStringed.r),
+    s: BigInt(sigStringed.s)
+  }
+
+  const hashMessage = (message) => keccak256(Uint8Array.from(message));
+
+  const isValid = secp.secp256k1.verify(sig, hashMessage(msg), sender) === true;
+  
+  if(!isValid) res.status(400).send({ message: "Bad signature!"});
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
